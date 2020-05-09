fe7e658e213982174598c960bc80eddadcffccc8
rosso0815 <rosso0815@gmail.com>
2020-05-09 18:20:13 +0200

    fix

---
+++ buildInstall.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+set -aeEux
+set -o pipefail
+
+
+echo "@@@ lets start"
+    #   - rm -rf tmp
+    #   - ls -al
+    #   - uname -a
+    #   - yum install -y wget
+    #   # - wget https://dl.google.com/go/go1.13.10.linux-amd64.tar.gz
+    #   # - ls -al
+echo "@@@ finish"
