af210286cb7a8554000b65083fc0781e6511bb6f
rosso0815 <rosso0815@gmail.com>
2020-05-09 18:23:00 +0200

    fix

--- buildInstall.sh
+++ buildInstall.sh
@@ -5,10 +5,10 @@ set -o pipefail
 
 
 echo "@@@ lets start"
-    #   - rm -rf tmp
-    #   - ls -al
-    #   - uname -a
-    #   - yum install -y wget
-    #   # - wget https://dl.google.com/go/go1.13.10.linux-amd64.tar.gz
-    #   # - ls -al
+rm -rf tmp
+uname -a
+yum install -y wget
+mkdir tmp
+wget https://dl.google.com/go/go1.13.10.linux-amd64.tar.gz
+rm -rf tmp
 echo "@@@ finish"
